6c2928f8e50c20575017e3e795436858f3f4eda2
DanielMC <matinzcorndani@hotmail.com>
2023-07-26 14:17:17 -0600

    Creating CSS documents

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,9 +1,11 @@
+import './TodoItem.css';
+
 function TodoItem({text}) {
   return (
-    <li>
-      <span>V</span>
-      <p>{text}</p>
-      <span>X</span>
+    <li className="TodoItem">
+      <span className="Icon-check Icon-check--activate"></span>
+      <p className="TodoItem-p TodoItem-p--complete">{text}</p>
+      <span className="Icon Icon-delete"></span>
     </li>
   );
 }
